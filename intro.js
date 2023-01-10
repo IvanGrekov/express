e7066b3b53fa2a35eb77cb6446c0f40b192be0ec
Ivan Grekov <ivan.grekov@windmill.digital>
2023-01-10 23:37:52 +0200

    intro express

--- intro.js
+++ intro.js
@@ -3,66 +3,74 @@ import express from 'express';
 
 console.clear();
 
-// const PORT = 3000;
+const PORT = 3000;
 // const server = new http.Server();
 
 // server.on('request', (req, res) => {
-//     res.end('<h1>Hello world!</h1>');
+//     res.end('<h1>Hello, world!</h1>');
+// });
+
+// server.on('error', (err) => {
+//     console.warn('Server error:', err.message);
 // });
 
 // server.listen(PORT, () => {
 //     console.log(`Server is running, http://localhost:${PORT}`);
 // });
 
-const PORT = 3000;
-const app = express();
+const server = express();
 
-//#region only for '/' with method 'GET'
-// app.get('/', (req, res) => {
+//#region only for '/index' with method 'GET'
+// server.get('/index', (req, res) => {
 //     res.end('<h1>Hello world!</h1>');
 // });
 //#endregion
 
-//#region only for '/' with method 'POST'
-// app.post('/', (req, res) => {
+//#region only for '/index' with method 'POST'
+// server.post('/index', (req, res) => {
 //     res.end('<h1>Hello world!</h1>');
-// });
-//#endregion
-
-//#region for '/about...' with any method
-// app.use('/about', (req, res) => {
-//     res.end('<h1>About page!</h1>');
 // });
 //#endregion
 
 //#region for '/...' with any method
-// app.use('/', (req, res) => {
+// server.use('/', (req, res) => {
 //     res.end('<h1>Home page!</h1>');
 // });
 //#endregion
 
+//#region for '/about...' with any method
+// server.use('/about', (req, res) => {
+//     res.end('<h1>About page!</h1>');
+// });
+//#endregion
+
 //#region break 'use' method
-// app.use('/', (req, res) => {
+// server.use('/', (req, res) => {
 //     res.end('<h1>Home page!</h1>');
 // });
 
-// NOTE: won't work because we already have matched url above
-// app.use('/team', (req, res) => {
+// NOTE: won't work even for 'http://localhost:3000/team' because we already have matched url above
+// server.use('/team', (req, res) => {
 //     res.end('<h1>About page!</h1>');
 // });
 //#endregion
 
 //#region prevent breaking for 'use' method
-// app.use('/', (req, res, next) => {
+// server.use('/', (req, res, next) => {
 //     res.write('<h1>Home page!</h1>');
+
+//     // NOTE: if there is no matching below, we won't end the response
+//     // NOTE: if there is a matching below, we will get error because we already made 'end'
+//     // NOTE: if there is a matching below and to prevent the error we should use 'write' instead of 'end' above
 //     next();
 // });
 
-// app.use('/team', (req, res) => {
+// server.use('/team', (req, res) => {
+//     // NOTE: if it will end the response if the request has '/team...' address
 //     res.end('<h1>About page!</h1>');
 // });
 //#endregion
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running, http://localhost:${PORT}`);
 });
--- request.js
+++ request.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:3000/json';
-axios.post(URL, { name: 'Ivan', surname: 'Grekov' }).then(({ data }) => console.log('data:', data));
+// const URL = 'http://localhost:3000';
+// axios.post(URL).then(({ data }) => console.log('data:', data));
+
+const URL = 'http://localhost:3000/team';
+axios.post(URL).then(({ data }) => console.log('data:', data));
