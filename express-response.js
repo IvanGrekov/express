32aa3fdd22e34e057da702f399cf9e0616b78105
Ivan Grekov <ivan.grekov@windmill.digital>
2023-01-14 14:48:01 +0200

    express-response

--- express-response.js
+++ express-response.js
@@ -6,46 +6,52 @@ console.clear();
 const PORT = 3000;
 const app = express();
 
-//#region send json
-// app.use('/json', (req, res) => {
-//     // NOTE: will be an error since we can't end with json
-//     // res.setHeader('Content-Type', 'application/json');
-//     // res.end({ name: 'Ivan' });
-
-//     res.send({ name: 'Ivan' });
+// app.use('/', (req, res) => {
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('Hello World');
 // });
-//#endregion
 
 //#region send html
 // app.use('/', (req, res) => {
-//     // res.setHeader('Content-Type', 'text/html');
-//     // res.end('Hello, world!');
+// res.setHeader('Content-Type', 'text/html');
+// res.end('Hello, world!');
 
-//     res.send('Hello, world!');
+// res.send('Hello, world!');
+// });
+//#endregion
+
+//#region send json
+// app.use('/json', (req, res) => {
+// NOTE: will be an error since we can't end with json
+// res.setHeader('Content-Type', 'application/json');
+// res.end({ name: 'Ivan' });
+
+// res.send({ name: 'Ivan' });
 // });
 //#endregion
 
 //#region send status with code description
 // app.use('/', (req, res) => {
-//     // res.sendStatus(200);
-//     // res.sendStatus(300);
-//     // res.sendStatus(400);
-//     // res.sendStatus(404);
-//     // res.sendStatus(500);
+// res.sendStatus(200);
+// res.sendStatus(300);
+// res.sendStatus(400);
+// res.sendStatus(404);
+// res.sendStatus(500);
 // });
 //#endregion
 
 //#region send file
 // app.use('/', (req, res) => {
-//     res.setHeader('Content-Disposition', 'attachment; filename="video.mp4');
-//     res.sendFile(path.resolve('public', 'lviv.mp4'));
+//     res.setHeader('Content-Disposition', 'attachment; filename=video.mp4');
+//     res.sendFile(path.resolve('public', 'panda.mp4'));
 // });
 //#endregion
 
 //#region static server
+// NOTE: we can pass relative path also
 // app.use(express.static(path.resolve('public')));
 
-// //NOTE: fallback
+//NOTE: fallback
 // app.use('/', (req, res) => {
 //     res.sendStatus(404);
 // });
